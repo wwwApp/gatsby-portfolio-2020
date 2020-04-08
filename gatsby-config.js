8825f13b3072e3b9b992e8953f97c481f7e6ccba
Woo Song <ws392@drexel.edu>
2020-04-08 04:45:40 -0400

    remove error-throwing plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
