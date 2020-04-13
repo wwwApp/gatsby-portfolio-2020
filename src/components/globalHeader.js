23177abdb25ec369319bc2f73b1e8b58847bcddf
Woo Song <ws392@drexel.edu>
2020-04-12 23:19:09 -0400

    fixes

--- src/components/globalHeader.js
+++ src/components/globalHeader.js
@@ -154,40 +154,36 @@ const GlobalHeader = () => {
   const headerRef = useRef(null)
 
   return (
-    <>
-      <Headroom
-        onPin={() => {
-          headerRef.current.classList.remove("is-not-pinned")
-          headerRef.current.classList.add("is-pinned")
-        }}
-        onUnpin={() => {
-          headerRef.current.classList.remove("is-pinned")
-          headerRef.current.classList.add("is-not-pinned")
-        }}
-      >
-        <header
-          className={`c-global-header ${isModalVisible ? "is-open" : ""}`}
-        >
-          <div className="l-container c-global-header__inner" ref={headerRef}>
-            <h1 className="u-sr-only">{siteMeta.title}</h1>
-            <Link className="c-global-header__logo" to="/">
-              <span className="u-sr-only">Wooyoung Song Logo</span>
-              <img src={withPrefix("/logo.png")} alt="Wooyoung Song Logo" />
-            </Link>
-            {contactEls()}
-            <button
-              className={`modal-toggle ${isModalVisible ? "is-open" : ""}`}
-              onClick={toggleModalVisible}
-            >
-              <span className="u-sr-only">
-                {isModalVisible ? "Close" : "Open"} Modal
-              </span>
-              <span className="modal-toggle__line modal-toggle__line--1"></span>
-              <span className="modal-toggle__line modal-toggle__line--2"></span>
-            </button>
-          </div>
-        </header>
-      </Headroom>
+    <Headroom
+      onPin={() => {
+        headerRef.current.classList.remove("is-not-pinned")
+        headerRef.current.classList.add("is-pinned")
+      }}
+      onUnpin={() => {
+        headerRef.current.classList.remove("is-pinned")
+        headerRef.current.classList.add("is-not-pinned")
+      }}
+    >
+      <header className={`c-global-header ${isModalVisible ? "is-open" : ""}`}>
+        <div className="l-container c-global-header__inner" ref={headerRef}>
+          <h1 className="u-sr-only">{siteMeta.title}</h1>
+          <Link className="c-global-header__logo" to="/">
+            <span className="u-sr-only">Wooyoung Song Logo</span>
+            <img src={withPrefix("/logo.png")} alt="Wooyoung Song Logo" />
+          </Link>
+          {contactEls()}
+          <button
+            className={`modal-toggle ${isModalVisible ? "is-open" : ""}`}
+            onClick={toggleModalVisible}
+          >
+            <span className="u-sr-only">
+              {isModalVisible ? "Close" : "Open"} Modal
+            </span>
+            <span className="modal-toggle__line modal-toggle__line--1"></span>
+            <span className="modal-toggle__line modal-toggle__line--2"></span>
+          </button>
+        </div>
+      </header>
       <div
         className={`c-global-header__inner--modal ${
           isModalVisible ? "is-open" : ""
@@ -196,7 +192,7 @@ const GlobalHeader = () => {
       >
         {contactEls()}
       </div>
-    </>
+    </Headroom>
   )
 }
 
